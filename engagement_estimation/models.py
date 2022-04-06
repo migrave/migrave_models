bc16067c063f6d7d78f03ddcc7c3d6cc002d3c4f
Mohammad Wasil <mwasil@outlook.co.id>
2022-04-06 13:04:21 +0200

    [engagement_estimation] change metrics to classification, use report from sklearn

--- engagement_estimation/models.py
+++ engagement_estimation/models.py
@@ -1,34 +1,44 @@
+import numpy as np
 from sklearn import metrics
+
 
 def sklearn(train_data,
             train_labels,
             test_data,
             test_labels,
-            classifier):
+            classifier,
+            target_names={0:-1, 1:0, 2:1}):
     """
-    Train generalized model: leave one out for test
+    Train classifier
+    Input:
+      train_data: train data
+      train_labels: train labels
+      test_data: test data
+      test_labels: test labels
+      target_names: an index mapping to labels
+    Return:
+      Classifier and dictionary containing the results
     """
 
-    classifier.fit(train_data.values, train_labels.values)
+    classifier.fit(train_data, train_labels)
+    scores = classifier.predict_proba(test_data)
+    predictions = [target_names[np.argmax(sc)] for sc in scores]
 
-    scores = classifier.predict_proba(test_data.values)[:,1]
-    pred = [round(value) for value in scores]   
-    accuracy = metrics.accuracy_score(test_labels, pred)
+    # classification report
+    cls_report = metrics.classification_report(test_labels,
+                                               predictions,
+                                               target_names=list(target_names.values()),
+                                               output_dict=True)
+    auroc = metrics.roc_auc_score(test_labels, scores, multi_class="ovr")
 
     result = {}
-    result["Accuracy"] = accuracy
-
-    auroc = metrics.roc_auc_score(test_labels, scores)
     result["AUROC"] = auroc
-
-    #set average to None to computer per-class precision
-    precision, recall, f1, _ = metrics.precision_recall_fscore_support(test_labels, 
-                                                                       pred, 
-                                                                       average=None, 
-                                                                       labels=[0,1])
-
-    result['Precision_0'], result['Precision_1'] = precision[0], precision[1]
-    result['Recall_0'], result['Recall_1'] = recall[0], recall[1]
-    result['F1_0'], result['F1_1'] = f1[0], f1[1]
+    for i,cls in enumerate(cls_report.keys()):
+        if cls in target_names.values():
+            result[f"Precision_{cls}"] = cls_report[cls]["precision"]
+            result[f"Recall_{cls}"] = cls_report[cls]["recall"]
+            result[f"F1_{cls}"] = cls_report[cls]["f1-score"]
+        elif cls == "accuracy":
+            result["Accuracy"] = cls_report[cls]
 
     return classifier, result
