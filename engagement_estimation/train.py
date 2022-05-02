20bebab7bfef78aa07c9c7edd2b0575f91cc8287
Alex Mitrevski <aleksandar.mitrevski@h-brs.de>
2022-05-02 14:58:42 +0200

    [engagement_estimation/train] Extended the docs

--- engagement_estimation/train.py
+++ engagement_estimation/train.py
@@ -1,3 +1,5 @@
+from typing import Sequence, Dict, List
+
 import os
 import models
 import utils
@@ -32,22 +34,29 @@ MIGRAVE_VISUAL_FEATURES = ['of_AU01_c', 'of_AU02_c', 'of_AU04_c', 'of_AU05_c',
 NON_FEATURES_COLS = ["participant", "session_num", "timestamp", "engagement"]
 
 
-def train_generalized_model(df_data,
+def train_generalized_model(df_data: pd.core.frame.DataFrame,
                             classifier,
-                            classifier_name,
-                            participants=[1,2,3,4],
-                            logdir="./logs"):
+                            classifier_name: str,
+                            participants: np.ndarray=[1,2,3,4],
+                            logdir: str="./logs") -> List[Dict]:
+    """Trains a generalized engagement model (i.e. a model trained on date from
+    multiple users). Evaluates the trained model using leave-one-out cross validation
+    (i.e. the model is evaluated on each participant separately).
+
+    Returns a list of dictionaries containing evaluation results (AUROC, precision, recall,
+    F-score, classification accuracy, training participants, and test participant), one
+    dictionary per participant; in other words, there are len(participants) dictionaries
+    in the resulting list.
+
+    Keyword arguments:
+    @param df_data: pd.core.frame.Dataframe -- dataset used for training and testing
+    @param classifier -- classifier used for training
+    @param classifier_name: str -- name of the classifier
+    @param participants: np.ndarray -- participant labels
+    @param logdir: str -- directory where the trained models and evaluation results are saved
+
     """
-    Train generalized model: leave one out for test
-    Input:
-      df_data: dataset in panda's dataframe format
-      classifier: classifier used for training
-      participats: labels
-      logdir: directory to save the model
-    Return:
-      Dictionary containing the results
-    """
-    all_results = []
+    evaluation_results = []
     for p in participants:
         # shuffle all data and reindex
         df_data = df_data.reindex(np.random.permutation(df_data.index))
@@ -55,15 +64,13 @@ def train_generalized_model(df_data,
 
         train_data, train_labels, test_data, test_labels, mean, std = utils.split_generalized_data(df_data,
                                                                                   idx=p)
-        model, result = models.sklearn(train_data.values,
-                                       train_labels.values,
-                                       test_data.values,
-                                       test_labels.values,
+        model, result = models.sklearn(train_data.values, train_labels.values,
+                                       test_data.values, test_labels.values,
                                        classifier)
         result['Train'] = ", ".join(str(x) for x in participants if x != p)
         result['Test'] = p
 
-        all_results.append(result)
+        evaluation_results.append(result)
 
         if not os.path.exists(logdir):
             os.makedirs(logdir)
@@ -71,27 +78,33 @@ def train_generalized_model(df_data,
         utils.save_classifier(classifier, mean, std,
                         "{}/generalized_{}_model_tested_on_{}.joblib".format(logdir, classifier_name, p))
 
-    return all_results
+    return evaluation_results
 
-def train_individualized_model(df_data,
+def train_individualized_model(df_data: pd.core.frame.DataFrame,
                                classifier,
-                               classifier_name,
-                               participants=[1,2,3,4],
-                               train_percentage=[0.8, 0.9],
-                               logdir="./logs"):
+                               classifier_name: str,
+                               participants: np.ndarray=[1,2,3,4],
+                               train_percentage: Sequence[float]=[0.8, 0.9],
+                               logdir: str="./logs") -> List[Dict]:
+    """Trains individualized models (one model for each participant),
+    using different train/test split percentages.
+
+    Returns a list of dictionaries containing evaluation results (AUROC, precision, recall,
+    F-score, classification accuracy, training participants, and test participant), one
+    dictionary per participant and train/test percentage split; in other words, there are
+        len(participants) * len(train_percentage)
+    dictionaries in the resulting list.
+
+    Keyword arguments:
+    @param df_data: pd.core.frame.Dataframe -- dataset used for training and testing
+    @param classifier -- classifier used for training
+    @param classifier_name: str -- name of the classifier
+    @param participants: np.ndarray -- participant labels
+    @param train_percentage: Sequence[float] -- a list of percentages used for training the model
+    @param logdir: str -- directory where the trained models and evaluation results are saved
+
     """
-    Train individualized model: train one model for each participant
-    starting from 10% of train data, and increment by 10% until 90% according to the paper
-    Input:
-      df_data: dataset in panda's dataframe format
-      classifier: classifier used for training
-      participats: labels
-      train_percentage: a list of percentages used for training the model
-      logdir: directory to save the model
-    Return:
-      Dictionary containing the results
-    """
-    all_results = []
+    evaluation_results = []
     for p in participants:
         # shuffle all data and reindex
         df_data = df_data.reindex(np.random.permutation(df_data.index))
@@ -102,37 +115,40 @@ def train_individualized_model(df_data,
             df_data = df_data.reset_index(drop=True)
 
             train_data, train_labels, test_data, test_labels, mean, std = utils.split_individualized_data(df_data,
-                                                                              idx=p,
-                                                                              train_percentage=tr_percentage)
+                                                                                                          idx=p,
+                                                                                                          train_percentage=tr_percentage)
 
-            model, result = models.sklearn(train_data.values,
-                                           train_labels.values,
-                                           test_data.values,
-                                           test_labels.values,
+            model, result = models.sklearn(train_data.values, train_labels.values,
+                                           test_data.values, test_labels.values,
                                            classifier)
             result['Participant'] = p
             result['Train'] = "{} ({})".format(p, int(tr_percentage*100))
             result['Test'] = "{} ({})".format(p, int((1-tr_percentage)*100))
 
-            all_results.append(result)
+            evaluation_results.append(result)
 
             if not os.path.exists(logdir):
                 os.makedirs(logdir)
 
             utils.save_classifier(classifier, mean, std,
-                        "{}/individualized_{}_trained_on_{}_train_percentage_{}.joblib".format(logdir,
-                                                                                               classifier_name,
-                                                                                               p,
-                                                                                               tr_percentage))
+                                  "{}/individualized_{}_trained_on_{}_train_percentage_{}.joblib".format(logdir,
+                                                                                                         classifier_name,
+                                                                                                         p,
+                                                                                                         tr_percentage))
 
-    return all_results
+    return evaluation_results
 
-def train(config_path, logdir="./logs"):
-    """
-    Helper function for training the model
-    Input:
-      config_path: path to the config file
-      logdir: directory where to store logs
+def train_and_evaluate(config_path: str, logdir: str="./logs") -> None:
+    """Trains and evaluates models as specified in the config file under "config_path".
+    Saves the results of the training and evaluation in the directory specified by "logdir".
+
+    Keyword arguments:
+    @param config_path: str -- path to a config file specifying training parameters,
+                               such as classifiers to train, model types (individualised
+                               and/or personalised, as well as the dataset to use
+                               for training and evaluation)
+    @param logdir: str -- directory where training logs and evaluation results will be stored
+                          (default './logs')
     """
 
     if not os.path.exists(logdir):
@@ -214,4 +230,4 @@ def train(config_path, logdir="./logs"):
 
 
 if __name__ == '__main__':
-    train(parser.parse_args().config)
+    train_and_evaluate(parser.parse_args().config)
