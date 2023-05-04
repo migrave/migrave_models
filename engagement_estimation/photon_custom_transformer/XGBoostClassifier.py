f5f5eb1d4de5c3b9048fdd3227da347bcf0aa740
julian24bas <jschanowski24@yahoo.de>
2023-05-04 17:03:35 +0200

    init hyperparameter tuning for XGBoost

---
+++ engagement_estimation/photon_custom_transformer/XGBoostClassifier.py
@@ -0,0 +1,44 @@
+from sklearn.model_selection import train_test_split
+from sklearn.base import BaseEstimator, ClassifierMixin
+import xgboost
+
+
+class XGBoostClassifier(BaseEstimator, ClassifierMixin):
+    def __init__(self, n_estimators=100, max_depth=6, subsample=.8, colsample_bytree=.8, learning_rate=.3, min_child_weight=1., reg_lambda=1., reg_alpha=.0, gamma=.0):
+        # it is important that you name your params the same in the constructor
+        #  stub as well as in your class variables!
+        self.n_estimators = n_estimators
+        self.max_depth = max_depth
+        self.subsample = subsample
+        self.colsample_bytree = colsample_bytree
+        self.learning_rate = learning_rate
+        self.min_child_weight = min_child_weight
+        self.reg_lambda = reg_lambda
+        self.reg_alpha = reg_alpha
+        self.gamma = gamma
+
+        self.model = None
+
+    def fit(self, X, y, targets=None, **kwargs):
+        self.model = xgboost.XGBClassifier(n_estimators=self.n_estimators,
+                                           max_depth=self.max_depth,
+                                           booster="gbtree",
+                                           n_jobs=-1,
+                                           eval_metric="logloss",
+                                           subsample=self.subsample,
+                                           colsample_bytree=self.colsample_bytree,
+                                           use_label_encoder=False,
+                                           learning_rate=self.learning_rate,
+                                           min_child_weight=self.min_child_weight,
+                                           reg_lambda=self.reg_lambda,
+                                           reg_alpha=self.reg_alpha,
+                                           gamma=self.gamma)
+        X, X_valid, y, y_valid = train_test_split(X, y, test_size=.1, stratify=y)
+        self.model.fit(X, y, eval_set=[(X_valid, y_valid)], early_stopping_rounds=10)
+        return self
+
+    def predict(self, X):
+        return self.model.predict(X)
+
+    def predict_proba(self, X):
+        return self.model.predict_proba(X)
---
+++ engagement_estimation/tune_parameters.py
@@ -0,0 +1,87 @@
+import numpy as np
+from sklearn.model_selection import LeaveOneGroupOut, StratifiedKFold
+from sklearn.metrics import recall_score, precision_score, f1_score, accuracy_score
+from photonai.base import Hyperpipe, PipelineElement, Switch
+from photonai.optimization import IntegerRange, FloatRange
+from photonai.base import PhotonRegistry
+import os
+
+from utils import merge_datasets, NON_FEATURES_COLS, NAN_MAX_COLS
+
+custom_element_root_folder = "./photon_custom_transformer"
+registry = PhotonRegistry(custom_elements_folder=custom_element_root_folder)
+
+# registry.register(photon_name="XGBoostClassifier",
+#                   class_str="XGBoostClassifier.XGBoostClassifier",
+#                   element_type="Transformer")
+
+registry.activate()
+
+# show information about the element
+# registry.info("XGBoostClassifier")
+
+modalities = ["video", "audio", "game"]
+datasets = ["features_video_right.csv", "features_video_left.csv", "features_video_color.csv"]
+
+
+def fill_nans(features):
+    features_copy = features.copy()
+    for col in features_copy.columns:
+        if not col.startswith(tuple(NAN_MAX_COLS)):
+            min_val = np.nanmin(features_copy[col])
+            features_copy[col] = features_copy[col].fillna(min_val)
+        else:
+            max_val = np.nanmax(features_copy[col])
+            features_copy[col] = features_copy[col].fillna(max_val)
+    return features_copy
+
+
+def f1_score_0(y_true, y_pred):
+    return f1_score(y_true, y_pred, average=None, zero_division=0)[0]
+
+
+def recall_score_0(y_true, y_pred):
+    return recall_score(y_true, y_pred, average=None, zero_division=0)[0]
+
+
+def precision_score_0(y_true, y_pred):
+    return precision_score(y_true, y_pred, average=None, zero_division=0)[0]
+
+
+pipe = Hyperpipe("migrave_pipe", project_folder="./logs/tuning",
+                 optimizer="random_grid_search",
+                 optimizer_params={"n_configurations": 50},
+                 metrics=["accuracy", "recall", "precision", "f1_score",
+                          ("f1_score_0", f1_score_0),
+                          ("recall_score_0", recall_score_0),
+                          ("precision_score_0", precision_score_0)],
+                 best_config_metric="f1_score_0",
+                 outer_cv=LeaveOneGroupOut(),
+                 inner_cv=StratifiedKFold(n_splits=10),
+                 use_test_set=True)
+
+pipe += Switch("StandardizationSwitch",
+               [PipelineElement("StandardScaler"), PipelineElement("MinMaxScaler")])
+
+pipe += PipelineElement("ImbalancedDataTransformer",
+                        hyperparameters={"method_name": ["RandomUnderSampler", "RandomOverSampler", "SMOTE"]})
+
+# hyperparamter gamma not used yet (reduces overfitting but unnecessary for already good hyperparameters)
+pipe += PipelineElement("XGBoostClassifier",
+                        hyperparameters={"n_estimators": IntegerRange(50, 500),
+                                         "max_depth": IntegerRange(3, 10),
+                                         "subsample": FloatRange(.5, 1.),
+                                         "colsample_bytree": IntegerRange(.5, 1.),
+                                         "learning_rate": FloatRange(.01, .3),
+                                         "min_child_weight": IntegerRange(1, 6),
+                                         "reg_lambda": FloatRange(.0, 5.),
+                                         "reg_alpha": FloatRange(.0, 5.),
+                                         "gamma": FloatRange(.0, 1.)})
+
+dataset_files = [os.path.join("dataset", dataset) for dataset in datasets]
+features, dataset_stems = merge_datasets(dataset_files, modalities)
+labels = features[["engagement"]].values.flatten()
+groups = features[["participant"]].values.flatten()
+features = fill_nans(features.drop(columns=NON_FEATURES_COLS)).values
+
+pipe.fit(features, labels, groups=groups)
